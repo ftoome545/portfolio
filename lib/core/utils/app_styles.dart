3785013a2629373fa6e0ddbbe6ce3c435b42bbd2
FatimaHure <ftoome7767@hotmail.com>
2025-04-17 16:00:25 +0300

    Fix index.html error and add app styles and font family

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+import 'package:practice_1/core/utils/size_config.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular16(context) {
+    return TextStyle(
+      color: const Color(0xFFC7C7C7),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular101(context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 101),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular28(context) {
+    return TextStyle(
+      color: const Color(0xFFC7C7C7),
+      fontSize: getResponsiveFontSize(context, fontSize: 28),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular32(context) {
+    return TextStyle(
+      color: const Color(0xFFC7C7C7),
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular57(context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 57),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular43(context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 43),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular76(context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 76),
+      fontFamily: 'Bebas Neue',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium12(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium16(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFC7C7C7),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium14(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium18(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium32(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMedium24(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleInterMedium16(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFC7C7C7),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleBold14(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFF0A0A0A),
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleBold16(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFF0A0A0A),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleSemibold16(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemibold18(BuildContext context) {
+    return TextStyle(
+      color: const Color(0xffD3E97A),
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'Manrope',
+      fontWeight: FontWeight.w600,
+    );
+  }
+}
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  // var dispatcher = PlatformDispatcher.instance;
+  // var physicalWidth = dispatcher.views.first.physicalSize.width;
+  // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  // double width = physicalWidth / devicePixelRatio;
+
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < SizeConfig.tablet) {
+    return width / 550;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1000;
+  } else {
+    return width / 1920;
+  }
+}
--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:practice_1/core/utils/app_colors.dart';
+import 'package:practice_1/core/utils/app_styles.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Text(
-          'Fatima Hure',
-          style: TextStyle(color: AppColors.secondryColor, fontSize: 30),
-        ),
-        Spacer(),
-        Text(
-          'Home',
-          style: TextStyle(color: AppColors.secondryColor, fontSize: 16),
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        Text(
-          'About',
-          style: TextStyle(color: AppColors.secondryColor, fontSize: 16),
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        Text(
-          'Contact',
-          style: TextStyle(color: AppColors.secondryColor, fontSize: 16),
-        ),
-      ],
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Fatima Hure',
+            style: AppStyles.styleRegular32(context),
+          ),
+          Row(
+            children: [
+              Text(
+                'Home',
+                style: AppStyles.styleInterMedium16(context),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(
+                'About',
+                style: AppStyles.styleInterMedium16(context),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(
+                'Contact',
+                style: AppStyles.styleInterMedium16(context),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
